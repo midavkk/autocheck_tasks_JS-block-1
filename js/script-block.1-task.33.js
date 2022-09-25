8946f26ae9c8efeb31d9fb1ec2051c822a3d6854
Vadym Kalinchenkov <midavkk@gmail.com>
2022-09-25 19:54:06 +0300

    Update script-block.1-task.33.js

---
+++ js/script-block.1-task.33.js
@@ -0,0 +1,32 @@
+function formatMessage(message, maxLength) {
+  let result =
+    message.length > maxLength ? message.slice(0, maxLength) + "..." : message;
+  // Change code below this line
+
+  /// Change code above this line
+  return result;
+}
+console.log(formatMessage("Curabitur ligula sapien", 16));
+console.log(formatMessage("Curabitur ligula sapien", 23));
+console.log(formatMessage("Vestibulum facilisis purus nec", 20));
+console.log(formatMessage("Vestibulum facilisis purus nec", 30));
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15));
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41));
+
+// function formatMessage(message, maxLength) {
+//   let result;
+//   // Change code below this line
+//   if (message.length > maxLength) {
+//     result = message.slice(0, maxLength) + "...";
+//   } else {
+//     result = message;
+//   }
+//   /// Change code above this line
+//   return result;
+// }
+// console.log(formatMessage("Curabitur ligula sapien", 16));
+// console.log(formatMessage("Curabitur ligula sapien", 23));
+// console.log(formatMessage("Vestibulum facilisis purus nec", 20));
+// console.log(formatMessage("Vestibulum facilisis purus nec", 30));
+// console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15));
+// console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41));
