512aac005076431cf1e03b7021bd38b7fd0d08aa
Vadym Kalinchenkov <midavkk@gmail.com>
2022-09-25 14:23:00 +0300

    Update script-block.1-task.31.js

---
+++ js/script-block.1-task.31.js
@@ -0,0 +1,12 @@
+const courseTopic = "JavaScript essentials";
+// Change code below this line
+
+const courseTopicLength = courseTopic.length;
+const firstElement = courseTopic[0];
+const lastElement = courseTopic[21 - 1];
+
+console.log(courseTopicLength);
+console.log(firstElement);
+console.log(lastElement);
+
+// Change code above this line
