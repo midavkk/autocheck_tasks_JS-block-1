f96747d17d9a9ce7520371f5a9df97ebf57e4296
Vadym Kalinchenkov <midavkk@gmail.com>
2022-09-26 23:24:29 +0300

    rename block.1 --> block.2

--- js/script-block.1-task.34.js
+++
@@ -1,8 +0,0 @@
-function normalizeInput(input) {
-  const normalizedInput = input.toLowerCase(); // Change this line
-
-  return normalizedInput;
-}
-console.log(normalizeInput("Hello world"));
-console.log(normalizeInput("This ISN'T SpaM"));
-console.log(normalizeInput("Big SALE"));
