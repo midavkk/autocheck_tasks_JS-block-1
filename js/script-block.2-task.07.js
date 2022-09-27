55fb186a50d384df9f0e0a008a46841fce92cc78
Vadym Kalinchenkov <midavkk@gmail.com>
2022-09-28 00:23:45 +0300

    Update script-block.2-task.07.js

--- js/script-block.2-task.07.js
+++ js/script-block.2-task.07.js
@@ -1,1 +1,10 @@
+// Объяви переменную fruitsArrayLength
+// и присвой ей длину массива fruits используя свойство length.
+// Объявлена переменная fruitsArrayLength
+// Значение переменной fruitsArrayLength это число 4
 
+const fruits = ["apple", "peach", "pear", "banana"];
+
+// Change code below this line
+const fruitsArrayLength = fruits.length;
+console.log(fruitsArrayLength);
