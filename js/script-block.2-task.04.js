1110f6fee2b8f1f6bc2d11b8ba73d92c085c2f54
Vadym Kalinchenkov <midavkk@gmail.com>
2022-09-28 00:01:45 +0300

    Update script-block.2-task.04.js

--- js/script-block.2-task.04.js
+++ js/script-block.2-task.04.js
@@ -1,1 +1,9 @@
+// Объяви переменную fruits и присвой ей массив фруктов -
+// строк "apple", "plum", "pear" и "orange".
+// 1. Объявлена переменная fruits
+// 2. Значение переменной fruits это массив ["apple", "plum", "pear", "orange"]
 
+// Change code below this line
+const fruits = ["apple", "plum", "pear", "orange"];
+
+console.log(fruits);
