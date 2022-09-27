74e22464d7c6c3c8d157ab8f3cbf2c7120e80ffe
Vadym Kalinchenkov <midavkk@gmail.com>
2022-09-28 00:01:48 +0300

    Update script-block.2-task.05.js

--- js/script-block.2-task.05.js
+++ js/script-block.2-task.05.js
@@ -1,1 +1,16 @@
+// Объяви три переменные и присвой каждой из них значение, используя нотацию квадратных скобок.
+// Имя переменной ---> Значение переменной
+// firstElement ---> первый элемент массива
+// secondElement ---> второй элемент массива
+// lastElement ---> последний элемент массива
+// 1. Объявлена переменная firstElement
+// 2. Значение переменной firstElement это строка "apple"
+// 3. Объявлена переменная secondElement
+// 4. Значение переменной secondElement это строка "plum"
+// 5. Объявлена переменная lastElement
+// 6. Значение переменной lastElement это строка "orange"
 
+const fruits = ["apple", "plum", "pear", "orange"];
+
+// Change code below this line
+console.log(fruits);
