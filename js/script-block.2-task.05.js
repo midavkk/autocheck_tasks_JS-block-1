9995e7f5992680f9ad2adddd3240cd5463b41e68
Vadym Kalinchenkov <midavkk@gmail.com>
2022-09-28 00:06:11 +0300

    Update script-block.2-task.05.js

--- js/script-block.2-task.05.js
+++ js/script-block.2-task.05.js
@@ -13,4 +13,10 @@
 const fruits = ["apple", "plum", "pear", "orange"];
 
 // Change code below this line
-console.log(fruits);
+const firstElement = fruits[0];
+const secondElement = fruits[1];
+const lastElement = fruits[3];
+
+console.log(firstElement);
+console.log(secondElement);
+console.log(lastElement);
