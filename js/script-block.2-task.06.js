3f238aa508f5860596759a2e0ebd6e485a52c772
Vadym Kalinchenkov <midavkk@gmail.com>
2022-09-28 00:23:41 +0300

    Update script-block.2-task.06.js

--- js/script-block.2-task.06.js
+++ js/script-block.2-task.06.js
@@ -1,1 +1,14 @@
+// Переопредели значения элементов с индексами 1 и 3.
+// Замени "plum" на "peach", а "orange" на "banana".
+// 1. Объявлена переменная fruits
+// 2. Значение переменной fruits это массив ["apple", "peach", "pear", "banana"]
+// 3. Замени "plum" на "peach"
+// 4. Замени "orange" на "banana".
 
+const fruits = ["apple", "plum", "pear", "orange"];
+
+// Write your code under this line
+fruits[1] = "peach";
+fruits[3] = "banana";
+
+console.log(fruits);
