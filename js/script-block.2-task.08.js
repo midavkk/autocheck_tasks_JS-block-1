e4a235147c130f5608e5deddee14f8132d72a710
Vadym Kalinchenkov <midavkk@gmail.com>
2022-09-28 00:40:03 +0300

    Update script-block.2-task.08.js

--- js/script-block.2-task.08.js
+++ js/script-block.2-task.08.js
@@ -1,1 +1,17 @@
+//Объяви две переменные:
+// Имя переменной ---> Ожидаемое значение
+// lastElementIndex ---> Индекс последнего элемента масcива fruits
+//                      через длина_массива - 1
+// lastElement ---> Значение последнего элемента массива
+// 1. Объявлена переменная lastElementIndex
+// 2. Значение переменной lastElementIndex это число 3
+// 3. Объявлена переменная lastElement
+// 4. Значение переменной lastElement это строка "banana"
 
+const fruits = ["apple", "peach", "pear", "banana"];
+
+// Change code below this line
+const lastElementIndex = fruits.length - 1;
+const lastElement = fruits[lastElementIndex];
+console.log(lastElementIndex);
+console.log(lastElement);
