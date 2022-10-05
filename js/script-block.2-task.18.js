ca2586f94bd9844fd468bb55c7657afec68384ad
Vadym Kalinchenkov <midavkk@gmail.com>
2022-10-05 15:14:01 +0300

    Update script-block.2-task.18.js

--- js/script-block.2-task.18.js
+++ js/script-block.2-task.18.js
@@ -1,1 +1,31 @@
+// Напиши функцию calculateTotal(number), которая принимает целое число(параметр number)
+// и возвращает сумму всех целых чисел от единицы и до этого числа.
+//     Например, если number равно 3, то сумма это 1 + 2 + 3, то есть 6.
+// Объявлена функция calculateTotal(number)
+// Вызов функции calculateTotal(1) возвращает 1
+// Вызов функции calculateTotal(3) возвращает 6
+// Вызов функции calculateTotal(7) возвращает 28
+// Вызов функции calculateTotal(18) возвращает 171
 
+function calculateTotal(number) {
+  // Change code below this line
+  let sum = 0;
+  for (let j = 1; j <= number; j += 1) {
+    sum += j;
+  }
+  // Change code above this line
+  return sum;
+}
+console.log(calculateTotal(1));
+console.log(calculateTotal(3));
+console.log(calculateTotal(7));
+console.log(calculateTotal(18));
+
+// const target = 3;
+// let sum = 0;
+
+// for (let i = 0; i <= target; i += 1) {
+//   sum += i;
+// }
+
+// console.log(sum);
