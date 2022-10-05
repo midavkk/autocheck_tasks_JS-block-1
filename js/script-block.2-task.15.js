b8f2b54151f8c068ea54381a95be1633a6bd3d2b
Vadym Kalinchenkov <midavkk@gmail.com>
2022-10-05 12:25:52 +0300

    Update script-block.2-task.15.js

--- js/script-block.2-task.15.js
+++ js/script-block.2-task.15.js
@@ -1,1 +1,17 @@
+// Дополни код так, чтобы в переменной allClients получился массив всех
+//     элементов массивов oldClients и newClients.
+// Объявлена переменная oldClients
+// Значение переменной oldClients это массив ["Mango", "Ajax", "Poly", "Kiwi"]
+// Объявлена переменная newClients
+// Значение переменной newClients это массив ["Peach", "Houston"]
+// Объявлена переменная allClients
+// Значение переменной allClients это массив ["Mango", "Ajax", "Poly", "Kiwi", "Peach", "Houston"]
+// Переменной allClients присвоен массив после применения метода concat
+// с правильными аргументами
 
+const oldClients = ["Mango", "Ajax", "Poly", "Kiwi"];
+const newClients = ["Peach", "Houston"];
+
+const allClients = oldClients.concat(newClients); // Change this line
+
+console.log(allClients);
