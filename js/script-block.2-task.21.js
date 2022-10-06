75e283415e86bd6dd11a0b584e30ae4a5d04e4ad
Vadym Kalinchenkov <midavkk@gmail.com>
2022-10-06 14:46:01 +0300

    Update script-block.2-task.21.js

--- js/script-block.2-task.21.js
+++ js/script-block.2-task.21.js
@@ -1,5 +1,28 @@
-const string = "The quick brown fox jumped over the lazy dog";
+// Напиши функцию findLongestWord(string) которая принимает произвольную строку
+// состоящую только из слов разделённых пробелом(параметр string) и
+// возвращает самое длинное слово в этой строке.
 
-for (const character of string) {
-  console.log(character);
+// Объявлена функция findLongestWord(string)
+// Вызов функции findLongestWord("The quick brown fox jumped over the lazy dog")
+// возвращает jumped
+// Вызов функции findLongestWord("Google do a roll") возвращает Google
+// Вызов функции findLongestWord("May the force be with you") возвращает force
+// Вызов функции findLongestWord() со случайной строкой возвращает правильное значение
+
+function findLongestWord(string) {
+  // Change code below this line
+  let words = string.split(" ");
+  let longWord = words[0];
+  for (const word of words) {
+    if (word.length > longWord.length) {
+      longWord = word;
+    }
+  }
+  return longWord;
+  // Change code above this line
 }
+
+console.log(findLongestWord("The quick brown fox jumped over the lazy dog"));
+console.log(findLongestWord("Google do a roll"));
+console.log(findLongestWord("May the force be with you"));
+console.log(findLongestWord("Собака лает, а караван идёт"));
