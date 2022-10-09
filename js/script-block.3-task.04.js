031f7c7ca414546b0c7791ffffda8c85d75f348e
Vadym Kalinchenkov <midavkk@gmail.com>
2022-10-09 21:46:13 +0300

    Update script-block.3-task.04.js

--- js/script-block.3-task.04.js
+++ js/script-block.3-task.04.js
@@ -1,1 +1,56 @@
+// <----- EXAMPLE ----->
+// const user = {
+//   name: "Jacques Gluke",
+//   tag: "jgluke",
+//   location: {
+//     country: "Jamaica",
+//     city: "Ocho Rios",
+//   },
+//   hobbies: ["swiming", "music", "sci-fi"],
+// };
 
+// const location = user.location;
+// console.log(location); // Объект location
+
+// const country = user.location.country;
+// console.log(country); // "Jamaica"
+
+// const hobbies = user.hobbies;
+// console.log(hobbies); // ["swiming", "music", "sci-fi"]
+
+// const firstHobby = user.hobbies[0];
+// console.log(firstHobby); // "swiming"
+
+// const numberOfHobbies = user.hobbies.length;
+// console.log(numberOfHobbies); // 3
+
+// <----- TASK-5/41 ----->
+
+const apartment = {
+  imgUrl: "https://via.placeholder.com/640x480",
+  descr: "Spacious apartment in the city center",
+  rating: 4,
+  price: 2153,
+  tags: ["premium", "promoted", "top"],
+  owner: {
+    name: "Henry",
+    phone: "982-126-1588",
+    email: "henry.carter@aptmail.com",
+  },
+};
+
+// Change code below this line
+const ownerName = apartment.owner.name;
+const ownerPhone = apartment.owner.phone;
+const ownerEmail = apartment.owner.email;
+const numberOfTags = apartment.tags.length;
+const firstTag = apartment.tags[0];
+const lastTag = apartment.tags[2];
+// Change code above this line
+
+console.log(ownerName);
+console.log(ownerPhone);
+console.log(ownerEmail);
+console.log(numberOfTags);
+console.log(firstTag);
+console.log(lastTag);
