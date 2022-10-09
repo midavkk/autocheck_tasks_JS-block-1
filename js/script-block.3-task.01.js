122ac808b6401591e7e3de8d41f8575096436b33
Vadym Kalinchenkov <midavkk@gmail.com>
2022-10-09 12:51:37 +0300

    Update script-block.3-task.01.js

--- js/script-block.3-task.01.js
+++ js/script-block.3-task.01.js
@@ -1,8 +1,17 @@
-const book = {
-  title: "The Last Kingdom",
-  author: "Bernard Cornwell",
-  genres: ["historical prose", "adventure"],
-  public: true,
-  rating: 8.38,
+// const book = {
+//   title: "The Last Kingdom",
+//   author: "Bernard Cornwell",
+//   genres: ["historical prose", "adventure"],
+//   public: true,
+//   rating: 8.38,
+// };
+// console.log(book);
+
+const apartment = {
+  imgUrl: "https://via.placeholder.com/640x480",
+  descr: "Spacious apartment in the city center",
+  rating: 4,
+  price: 2153,
+  tags: ["premium", "promoted", "top"],
 };
-console.log(book);
+console.log(apartment);
