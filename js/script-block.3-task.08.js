81fa56b59ee03bf5299dff97439f40a6c972824e
Vadym Kalinchenkov <midavkk@gmail.com>
2022-10-10 19:18:23 +0300

    Update script-block.3-task.08.js

--- js/script-block.3-task.08.js
+++ js/script-block.3-task.08.js
@@ -1,1 +1,42 @@
+// const name = "Генри Сибола";
+// const age = 25;
 
+// const user = {
+//   name: name,
+//   age: age,
+// };
+
+// console.log(user.name); // "Генри Сибола"
+// console.log(user.age); // 25
+
+//  <<<----------------- OR ----------------->>>
+
+// const name = "Генри Сибола";
+// const age = 25;
+
+// const user = {
+//   name,
+//   age,
+// };
+
+// console.log(user.name); // "Генри Сибола"
+// console.log(user.age); // 25
+
+const name = "Repair Droid";
+const price = 2500;
+const image = "https://via.placeholder.com/640x480";
+const tags = ["on sale", "trending", "best buy"];
+
+const product = {
+  // Change code below this line
+  name,
+  price,
+  image,
+  tags,
+  // Change code above this line
+};
+
+console.log(product.name);
+console.log(product.price);
+console.log(product.image);
+console.log(product.tags);
