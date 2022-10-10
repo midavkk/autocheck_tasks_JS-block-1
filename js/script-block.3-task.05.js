6f2baf123df9484949d289a7e68a49684a95bce4
Vadym Kalinchenkov <midavkk@gmail.com>
2022-10-10 19:18:11 +0300

    Update script-block.3-task.05.js

--- js/script-block.3-task.05.js
+++ js/script-block.3-task.05.js
@@ -1,1 +1,37 @@
+// const book = {
+//   title: "The Last Kingdom",
+//   author: "Bernard Cornwell",
+//   genres: ["historical prose", "adventure"],
+//   public: true,
+//   rating: 8.38,
+// };
 
+// const bookTitle = book["title"];
+// console.log(bookTitle); // "The Last Kingdom"
+
+// const bookGenres = book["genres"];
+// console.log(bookGenres); // ["historical prose", "adventure"]
+
+// const propKey = "author";
+// const bookAuthor = book[propKey];
+// console.log(bookAuthor); // "Bernard Cornwell"
+
+const apartment = {
+  imgUrl: "https://via.placeholder.com/640x480",
+  descr: "Spacious apartment in the city center",
+  rating: 4,
+  price: 2153,
+  tags: ["premium", "promoted", "top"],
+};
+
+// Change code below this line
+const aptRating = apartment["rating"];
+const aptDescr = apartment["descr"];
+const aptPrice = apartment["price"];
+const aptTags = apartment["tags"];
+// Change code above this line
+
+console.log(aptRating);
+console.log(aptDescr);
+console.log(aptPrice);
+console.log(aptTags);
