c19c750383335fab3bd13ddee38a73123e9b34de
Vadym Kalinchenkov <midavkk@gmail.com>
2022-10-11 12:01:15 +0300

    Update script-block.3-task.10.js

--- js/script-block.3-task.10.js
+++ js/script-block.3-task.10.js
@@ -1,1 +1,29 @@
+// const book = {
+//   title: "The Last Kingdom",
+//   author: "Bernard Cornwell",
+//   genres: ["historical prose", "adventure"],
+//   rating: 8.38,
+// };
 
+// for (const key in book) {
+//   // Ключ
+//     console.log(key);
+//   // Значение свойства с таким ключом
+//   console.log(book[key]);
+// }
+
+const apartment = {
+  descr: "Spacious apartment in the city center",
+  rating: 4,
+  price: 2153,
+};
+const keys = [];
+const values = [];
+// Change code below this line
+for (const key in apartment) {
+  keys.push(key);
+  values.push(apartment[key]);
+}
+
+console.log(keys);
+console.log(values);
