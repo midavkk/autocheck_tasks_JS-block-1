95a433ec957d25d6846bc139b730781fe49932fc
Vadym Kalinchenkov <midavkk@gmail.com>
2022-10-11 12:01:17 +0300

    Update script-block.3-task.11.js

--- js/script-block.3-task.11.js
+++ js/script-block.3-task.11.js
@@ -1,1 +1,37 @@
+// const book = {
+//   title: "The Last Kingdom",
+//   author: "Bernard Cornwell",
+//   genres: ["historical prose", "adventure"],
+//   rating: 8.38,
+// };
 
+// for (const key in book) {
+//   // Если это собственное свойство - выполняем тело if
+//   if (book.hasOwnProperty(key)) {
+//     console.log(key);
+//     console.log(book[key]);
+//   }
+
+//   // Если это не собственное свойство - ничего не делаем
+// }
+
+const keys = [];
+const values = [];
+const advert = {
+  service: "apt",
+};
+const apartment = Object.create(advert);
+apartment.descr = "Spacious apartment in the city center";
+apartment.rating = 4;
+apartment.price = 2153;
+
+for (const key in apartment) {
+  // Change code below this line
+  if (apartment.hasOwnProperty(key)) {
+    keys.push(key);
+    values.push(apartment[key]);
+  }
+  // Change code above this line
+}
+console.log(keys);
+console.log(values);
