59bb1c0988f23a95b648919d3226dc7fb6e4d223
Vadym Kalinchenkov <midavkk@gmail.com>
2022-10-11 15:57:17 +0300

    Update script-block.3-task.14.js

--- js/script-block.3-task.14.js
+++ js/script-block.3-task.14.js
@@ -1,1 +1,36 @@
+//  <<<----------------- EXAMPLE ----------------->>>
 
+// function countProps(object) {
+//   // Change code below this line
+//   let propCount = 0;
+
+//   for (const key in object) {
+//     if (object.hasOwnProperty(key)) {
+//       propCount += 1;
+//     }
+//   }
+
+//   return propCount;
+//   // Change code above this line
+// }
+// console.log(countProps({}));
+// console.log(countProps({ name: "Mango", age: 2 }));
+// console.log(countProps({ mail: "poly@mail.com", isOnline: true, score: 500 }));
+
+// <<<----------------- TASK ----------------->>>
+
+function countProps(object) {
+  // Change code below this line
+  let propCount = 0;
+  const keys = Object.keys(object);
+  for (const key of keys) {
+    propCount += 1;
+  }
+
+  return propCount;
+  // Change code above this line
+}
+
+console.log(countProps({}));
+console.log(countProps({ name: "Mango", age: 2 }));
+console.log(countProps({ mail: "poly@mail.com", isOnline: true, score: 500 }));
