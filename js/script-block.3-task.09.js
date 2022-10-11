5f53376f009544de978dfac437f05ff2fee855a8
Vadym Kalinchenkov <midavkk@gmail.com>
2022-10-11 12:01:12 +0300

    Update script-block.3-task.09.js

--- js/script-block.3-task.09.js
+++ js/script-block.3-task.09.js
@@ -1,1 +1,33 @@
+// const propName = "name";
+// const user = {
+//   age: 25,
+// };
 
+// user[propName] = "Генри Сибола";
+// console.log(user.name); // "Генри Сибола"
+
+//  <<<----------------- CONTINUE ----------------->>>
+
+// const propName = "name";
+// const user = {
+//   age: 25,
+//   // Имя этого свойства будет взято из значения переменной propName
+//   [propName]: "Генри Сибола",
+// };
+
+// console.log(user.name); // "Генри Сибола"
+
+//  <<<----------------- TASK ----------------->>>
+
+const emailInputName = "email";
+const passwordInputName = "password";
+
+const credentials = {
+  // Change code below this line
+  [emailInputName]: "henry.carter@aptmail.com",
+  [passwordInputName]: "jqueryismyjam",
+  // Change code above this line
+};
+
+console.log(credentials.email);
+console.log(credentials.password);
