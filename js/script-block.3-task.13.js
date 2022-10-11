d9451067c4d924d057973d88cc4e38a9bc34c438
Vadym Kalinchenkov <midavkk@gmail.com>
2022-10-11 15:57:13 +0300

    Update script-block.3-task.13.js

--- js/script-block.3-task.13.js
+++ js/script-block.3-task.13.js
@@ -1,1 +1,42 @@
+//  <<<----------------- EXAMPLE ----------------->>>
 
+// const book = {
+//   title: "The Last Kingdom",
+//   author: "Bernard Cornwell",
+//   genres: ["historical prose", "adventure"],
+//   rating: 8.38,
+// };
+// const keys = Object.keys(book);
+// console.log(keys); // ['title', 'author', 'genres', 'rating']
+
+//  <<<----------------- EXAMPLE ----------------->>>
+
+// const book = {
+//   author: "Bernard Cornwell",
+//   genres: ["historical prose", "adventure"],
+//   rating: 8.38,
+// };
+// const keys = Object.keys(book);
+
+// for (const key of keys) {
+//   // Ключ
+//   console.log(key);
+//   // Значение свойства
+//   console.log(book[key]);
+// }
+
+// <<<----------------- TASK ----------------->>>
+
+const apartment = {
+  descr: "Spacious apartment in the city center",
+  rating: 4,
+  price: 2153,
+};
+const values = [];
+// Change code below this line
+const keys = Object.keys(apartment);
+for (const key of keys) {
+  values.push(apartment[key]);
+  console.log(key);
+}
+console.log(values);
