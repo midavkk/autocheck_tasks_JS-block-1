a1a4327482b0b29ba80bfd5b346dcabe91bd6cd5
Vadym Kalinchenkov <midavkk@gmail.com>
2022-10-12 21:26:22 +0300

    Update script-block.3-task.18.js

--- js/script-block.3-task.18.js
+++ js/script-block.3-task.18.js
@@ -1,1 +1,25 @@
+//  <<<----------------- EXAMPLE ----------------->>>
 
+// <<<----------------- TASK - ЗАДАЧА. ПОИСК ОБЪЕКТА ПО ЗНАЧЕНИЮ СВОЙСТВА ----------------->>
+const products = [
+  { name: "Radar", price: 1300, quantity: 4 },
+  { name: "Scanner", price: 2700, quantity: 3 },
+  { name: "Droid", price: 400, quantity: 7 },
+  { name: "Grip", price: 1200, quantity: 9 },
+];
+
+function getProductPrice(productName) {
+  // Change code below this line
+  for (const product of products) {
+    if (productName === product.name) {
+      return product.price;
+    }
+  }
+  return null;
+  // Change code above this line
+}
+console.log(getProductPrice("Radar"));
+console.log(getProductPrice("Grip"));
+console.log(getProductPrice("Scanner"));
+console.log(getProductPrice("Droid"));
+console.log(getProductPrice("Engine"));
