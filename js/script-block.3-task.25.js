985a9162ea5168548dfff17015faaf6663deac13
Vadym Kalinchenkov <midavkk@gmail.com>
2022-10-17 01:39:14 +0300

    Update script-block.3-task.25.js

--- js/script-block.3-task.25.js
+++ js/script-block.3-task.25.js
@@ -1,1 +1,66 @@
+// <<< ----------------- EXAMPLE ----------------->>>
 
+// const user = {
+//   name: "Jacques Gluke",
+//   tag: "jgluke",
+//   stats: {
+//     followers: 5603,
+//     views: 4827,
+//     likes: 1308,
+//   },
+// };
+
+// const {
+//   name,
+//   tag,
+//   stats: { followers, views: userViews, likes: userLikes = 0 },
+// } = user;
+
+// console.log(name); // Jacques Gluke
+// console.log(tag); // jgluke
+// console.log(followers); // 5603
+// console.log(userViews); // 4827
+// console.log(userLikes); // 1308
+
+// <<<----------------- TASK - ЗАДАЧА. ДЕСТРУКТУРИЗАЦИЯ ОБЪЕКТА - ДЕСТРУКТУРИЗАЦИЯ В ЦИКЛАХ ----------------->>
+
+const forecast = {
+  today: {
+    low: 28,
+    high: 32,
+    icon: "https://www.flaticon.com/svg/static/icons/svg/861/861059.svg",
+  },
+  tomorrow: {
+    low: 27,
+    high: 31,
+  },
+};
+// Change code below this line
+
+// const highToday = forecast.today.high;
+// const lowToday = forecast.today.low;
+// const todayIcon = forecast.today.icon;
+
+// const highTomorrow = forecast.tomorrow.high;
+// const lowTomorrow = forecast.tomorrow.low;
+// const tomorrowIcon = forecast.tomorrow.icon;
+
+const {
+  today: {
+    high: highToday,
+    low: lowToday,
+    icon: todayIcon = "https://www.flaticon.com/svg/static/icons/svg/861/861059.svg",
+  },
+  tomorrow: {
+    high: highTomorrow,
+    low: lowTomorrow,
+    icon: tomorrowIcon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg",
+  },
+} = forecast;
+
+console.log(highToday);
+console.log(lowToday);
+console.log(todayIcon);
+console.log(highTomorrow);
+console.log(lowTomorrow);
+console.log(tomorrowIcon);
