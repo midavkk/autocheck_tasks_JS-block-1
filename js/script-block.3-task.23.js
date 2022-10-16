1ff0a7e91361276b1cb81fe6d7d9d5687e256188
Vadym Kalinchenkov <midavkk@gmail.com>
2022-10-16 20:12:12 +0300

    Update script-block.3-task.23.js

--- js/script-block.3-task.23.js
+++ js/script-block.3-task.23.js
@@ -1,1 +1,53 @@
+// <<< ----------------- EXAMPLE----------------->>>
 
+// const firstBook = {
+//   title: "Последнее королевство",
+//   coverImage:
+//     "https://images-na.ssl-images-amazon.com/images/I/51b5YG6Y1rL.jpg",
+// };
+
+// const {
+//   title: firstTitle,
+//   coverImage: firstCoverImage = "https://via.placeholder.com/640/480",
+// } = firstBook;
+
+// console.log(firstTitle); // Последнее королевство
+// console.log(firstCoverImage); // https://images-na.ssl-images-amazon.com/images/I/51b5YG6Y1rL.jpg
+
+// <<< ----------------- EXAMPLE----------------->>>
+
+// const secondBook = {
+//   title: "Сон смешного человека",
+// };
+
+// const {
+//   title: secondTitle,
+//   coverImage: secondCoverImage = "https://via.placeholder.com/640/480",
+// } = secondBook;
+
+// console.log(secondTitle); // Сон смешного человека
+// console.log(secondCoverImage); // https://via.placeholder.com/640/480
+
+// <<<----------------- TASK - ЗАДАЧА. ДЕСТРУКТУРИЗАЦИЯ ОБЪЕКТА - ИЗМЕНЕНИЕ ИМЕНИ ПЕРЕМЕННОЙ ----------------->>
+
+const highTemperatures = {
+  yesterday: 28,
+  today: 26,
+  tomorrow: 33,
+};
+// Change code below this line
+
+// const highYesterday = highTemperatures.yesterday;
+// const highToday = highTemperatures.today;
+// const highTomorrow = highTemperatures.tomorrow;
+// const highIcon = highTemperatures.icon;
+
+const {
+  yesterday: highYesterday,
+  today: highToday,
+  tomorrow: highTomorrow,
+  icon: highIcon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg",
+} = highTemperatures;
+// Change code above this line
+const meanTemperature = (highYesterday + highToday + highTomorrow) / 3;
+console.log(meanTemperature);
