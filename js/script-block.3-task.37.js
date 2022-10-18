ca4bcbc48ff811dcabc2c97046138546b58e65a0
Vadym Kalinchenkov <midavkk@gmail.com>
2022-10-19 00:40:16 +0300

    Update script-block.3-task.37.js

--- js/script-block.3-task.37.js
+++ js/script-block.3-task.37.js
@@ -1,1 +1,10 @@
-
+// <<<----------------- TASK - ЗАДАЧА. ПОЛУЧАЕМ ВСЕ ЗЕЛЬЯ ----------------->>
+const atTheOldToad = {
+  potions: ["Speed potion", "Dragon breath", "Stone skin"],
+  // Change code below this line
+  getPotions() {
+    return this.potions;
+  },
+  // Change code above this line
+};
+console.log(atTheOldToad.getPotions());
