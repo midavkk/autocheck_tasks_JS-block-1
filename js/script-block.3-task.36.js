2912ed7792e334e4afc2c72b39b5bc9df165b0b9
Vadym Kalinchenkov <midavkk@gmail.com>
2022-10-19 00:40:11 +0300

    Update script-block.3-task.36.js

--- js/script-block.3-task.36.js
+++ js/script-block.3-task.36.js
@@ -1,1 +1,7 @@
-
+// <<<----------------- TASK - ЗАДАЧА. ЛАВКА ЗЕЛИЙ «У СТАРОЙ ЖАБЫ» ----------------->>
+const atTheOldToad = {
+  // Change code below this line
+  potions: [],
+  // Change code above this line
+};
+console.log(atTheOldToad.potions);
