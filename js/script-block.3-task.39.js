73db83df9c28bc298da453f30df93cfb84991ce3
Vadym Kalinchenkov <midavkk@gmail.com>
2022-10-20 16:54:17 +0300

    Update script-block.3-task.39.js

--- js/script-block.3-task.39.js
+++ js/script-block.3-task.39.js
@@ -1,1 +1,13 @@
-
+// <<<----------------- TASK - ЗАДАЧА. УДАЛЯЕМ ЗЕЛЬЕ ----------------->>
+const atTheOldToad = {
+  potions: ["Speed potion", "Dragon breath", "Stone skin"],
+  removePotion(potionName) {
+    // Change code below this line
+    potionName = this.potions.indexOf(potionName);
+    this.potions.splice(potionName, 1);
+    return this.potions;
+    // Change code above this line
+  },
+};
+console.log(atTheOldToad.removePotion("Dragon breath"));
+console.log(atTheOldToad.removePotion("Speed potion"));
