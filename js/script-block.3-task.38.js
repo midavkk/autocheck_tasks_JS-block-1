b88b8a45f2f779ef04cbe14a73772f4fce69abbe
Vadym Kalinchenkov <midavkk@gmail.com>
2022-10-20 16:54:10 +0300

    Update script-block.3-task.38.js

--- js/script-block.3-task.38.js
+++ js/script-block.3-task.38.js
@@ -1,1 +1,12 @@
-
+// <<<----------------- TASK - ЗАДАЧА. ДОБАВЛЯЕМ НОВОЕ ЗЕЛЬЕ ----------------->>
+const atTheOldToad = {
+  potions: ["Speed potion", "Dragon breath", "Stone skin"],
+  addPotion(potionName) {
+    // Change code below this line
+    this.potions.push(potionName);
+    return this.potions;
+    // Change code above this line
+  },
+};
+console.log(atTheOldToad.addPotion("Invisibility"));
+console.log(atTheOldToad.addPotion("Power potion"));
