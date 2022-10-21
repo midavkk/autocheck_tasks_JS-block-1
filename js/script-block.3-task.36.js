79e25d0656958d464454400e70eb577191af28ed
Vadym Kalinchenkov <midavkk@gmail.com>
2022-10-21 14:30:25 +0300

    Delete .js fales

--- js/script-block.3-task.36.js
+++
@@ -1,7 +0,0 @@
-// <<<----------------- TASK - ЗАДАЧА. ЛАВКА ЗЕЛИЙ «У СТАРОЙ ЖАБЫ» ----------------->>
-const atTheOldToad = {
-  // Change code below this line
-  potions: [],
-  // Change code above this line
-};
-console.log(atTheOldToad.potions);
