b6aebc3b8d1784692ae6d06cea8d5035d751ae05
Vadym Kalinchenkov <midavkk@gmail.com>
2022-10-21 14:52:31 +0300

    Update script-block.4-task.01.js

--- js/script-block.4-task.01.js
+++ js/script-block.4-task.01.js
@@ -1,2 +1,30 @@
 // <<<------------- EXAMPLE ------------->>>
-// <<<------------- TASK ------------->>>
+// function greet(name) {
+//   return `Добро пожаловать ${name}.`;
+// }
+// // Вызываем функцию greet
+// // и выводим результат в консоль
+// console.log(greet("Mango"));
+// // Добро пожаловать Mango.
+// // Выводим функцию greet
+// // в консоль не вызывая её
+// console.log(greet);
+// /*
+// ƒ greet() {
+//      return `Добро пожаловать ${name}.`;
+// }
+// */
+
+// <<<------------- TASK ЗАДАЧА. ФУНКЦИЯ КАК ЗНАЧЕНИЕ ------------->>>
+function makePizza() {
+  return "Your pizza is being prepared, please wait.";
+}
+// Change code below this line
+
+const result = null;
+const pointer = null;
+
+makePizza(result);
+console.log(makePizza(result));
+makePizza(pointer);
+console.log(makePizza);
