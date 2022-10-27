3f953109a01fc0a0992a704eefa430b0046d058a
Vadym Kalinchenkov <midavkk@gmail.com>
2022-10-27 16:51:32 +0300

    Update script-block.4-task.02.js

--- js/script-block.4-task.02.js
+++ js/script-block.4-task.02.js
@@ -1,2 +1,30 @@
 // <<<------------- EXAMPLE ------------->>>
+
+// // Колбэк-функция
+// function greet(name) {
+//   console.log(`Добро пожаловать ${name}.`);
+// }
+
+// // Функция высшего порядка
+// function registerGuest(name, callback) {
+//   console.log(`Регистрируем гостя ${name}.`);
+//   callback(name);
+// }
+
+// registerGuest("Mango", greet);
+
 // <<<------------- TASK ------------->>>
+function deliverPizza(pizzaName) {
+  return `Delivering ${pizzaName} pizza.`;
+}
+
+function makePizza(pizzaName) {
+  return `Pizza ${pizzaName} is being prepared, please wait...`;
+}
+
+// Chande code below this line
+function makeMessage(pizzaName, callback) {
+  return callback(pizzaName);
+}
+console.log(makeMessage("Royal Grand", makePizza));
+console.log(makeMessage("Ultracheese", deliverPizza));
