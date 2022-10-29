26afa32cbaf2664d0a76b268ec9fdea6a213e2d1
Vadym Kalinchenkov <midavkk@gmail.com>
2022-10-29 13:45:33 +0300

    Update script-block.4-task.06.js

--- js/script-block.4-task.06.js
+++ js/script-block.4-task.06.js
@@ -1,2 +1,25 @@
 // <<<------------- EXAMPLE ------------->>>
-// <<<------------- TASK ------------->>>
+// <<<------------- TASK - МЕТОД FOREACH(CALLBACK) ------------->>>
+function filterArray(numbers, value) {
+  const filteredNumbers = [];
+  // Change code below this line
+
+  //   for (let i = 0; i < numbers.length; i += 1) {
+  //     if (numbers[i] > value) {
+  //       filteredNumbers.push(numbers[i]);
+  //     }
+
+  numbers.forEach(function (el) {
+    if (el > value) {
+      filteredNumbers.push(el);
+    }
+  });
+
+  // Change code above this line
+  return filteredNumbers;
+}
+console.log(filterArray([1, 2, 3, 4, 5], 3));
+console.log(filterArray([1, 2, 3, 4, 5], 4));
+console.log(filterArray([1, 2, 3, 4, 5], 5));
+console.log(filterArray([12, 24, 8, 41, 76], 38));
+console.log(filterArray([12, 24, 8, 41, 76], 20));
