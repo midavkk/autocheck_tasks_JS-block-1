8c834e2c927ba6d379a6c7d6c013135e2e4c2325
Vadym Kalinchenkov <midavkk@gmail.com>
2022-11-03 22:12:22 +0200

    Update script-block.4-task.10.js

--- js/script-block.4-task.10.js
+++ js/script-block.4-task.10.js
@@ -1,2 +1,32 @@
 // <<<------------- EXAMPLE ------------->>>
-// <<<------------- TASK ------------->>>
+const numbers = [5, 10, 15, 20, 25];
+
+// Объявление функции
+// numbers.forEach(function (number, index) {
+//   console.log(`Индекс ${index}, значение ${number}`);
+// });
+
+// Анонимная стрелочная функция
+// numbers.forEach((number, index) => {
+//   console.log(`Индекс ${index}, значение ${number}`);
+// });
+
+// <<<------------- TASK - СТРЕЛОЧНЫЕ ФУНКЦИИ КАК КОЛЛБЕКИ ------------->>>
+// Change code below this line
+// function calculateTotalPrice(orderedItems)
+const calculateTotalPrice = (orderedItems) => {
+  let totalPrice = 0;
+
+  //   orderedItems.forEach(function (item) {
+  //     totalPrice += item;
+  //   });
+  orderedItems.forEach((item) => {
+    totalPrice += item;
+  });
+
+  return totalPrice;
+};
+// Change code above this line
+console.log(calculateTotalPrice([12, 85, 37, 4]));
+console.log(calculateTotalPrice([164, 48, 291]));
+console.log(calculateTotalPrice([412, 371, 94, 63, 176]));
