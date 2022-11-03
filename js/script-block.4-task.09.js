b818062bf1debf811413851b16d511484a2fa2f0
Vadym Kalinchenkov <midavkk@gmail.com>
2022-11-03 22:12:18 +0200

    Update script-block.4-task.09.js

--- js/script-block.4-task.09.js
+++ js/script-block.4-task.09.js
@@ -1,2 +1,23 @@
 // <<<------------- EXAMPLE ------------->>>
-// <<<------------- TASK ------------->>>
+// const add = (a, b, c) => {
+//   console.log(a, b, c);
+//   return a + b + c;
+// };
+// console.log(add(3, 4, 5));
+
+// const add2 = (x, y, z) => x + y + z;
+// console.log(add2(7, 8, 9));
+
+// <<<------------- TASK - НЕЯВНЫЙ ВОЗВРАТ ------------->>>
+// Change code below this line
+// const calculateTotalPrice = (quantity, pricePerItem) => {
+//   return quantity * pricePerItem;
+// };
+
+const calculateTotalPrice = (quantity, pricePerItem) => quantity * pricePerItem;
+
+// Change code above this line
+
+console.log(calculateTotalPrice(5, 100));
+console.log(calculateTotalPrice(8, 60));
+console.log(calculateTotalPrice(3, 400));
