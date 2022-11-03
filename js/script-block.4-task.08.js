1113d86c8298b7080c6eedeb406273028f748c54
Vadym Kalinchenkov <midavkk@gmail.com>
2022-11-03 22:12:15 +0200

    Update script-block.4-task.08.js

--- js/script-block.4-task.08.js
+++ js/script-block.4-task.08.js
@@ -1,2 +1,25 @@
 // <<<------------- EXAMPLE ------------->>>
-// <<<------------- TASK ------------->>>
+// // Обычное объявление функции
+// function classicAdd(a, b, c) {
+//   return a + b + c;
+// }
+
+// // Тоже самое как стрелочная функция
+// const arrowAdd = (a, b, c) => {
+//   return a + b + c;
+// };
+// console.log(arrowAdd(4, 2, 3)); // 9
+
+// <<<------------- TASK - СТРЕЛОЧНЫЕ ФУНКЦИИ. ------------->>>
+// Change code below this line
+
+// function calculateTotalPrice(quantity, pricePerItem) {
+//   // Change code above this line
+//   return quantity * pricePerItem;
+// }
+
+const calculateTotalPrice = (quantity, pricePerItem) => {
+  return quantity * pricePerItem;
+};
+
+console.log(calculateTotalPrice(5, 20));
