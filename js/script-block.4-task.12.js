8dff8a44919ba547da96caf319d484f29ddb050f
Vadym Kalinchenkov <midavkk@gmail.com>
2022-11-10 19:07:03 +0200

    Update script-block.4-task.12.js

--- js/script-block.4-task.12.js
+++ js/script-block.4-task.12.js
@@ -1,2 +1,22 @@
 // <<<------------- EXAMPLE ------------->>>
+
 // <<<------------- TASK ------------->>>
+
+// Change code below this line
+const getCommonElements = (firstArray, secondArray) => {
+  const commonElements = [];
+
+  firstArray.forEach((element) => {
+    if (secondArray.includes(element)) {
+      commonElements.push(element);
+    }
+  });
+
+  // Change code above this line
+  return commonElements;
+};
+console.log(getCommonElements([1, 2, 3], [2, 4]));
+console.log(getCommonElements([1, 2, 3], [2, 1, 17, 19]));
+console.log(getCommonElements([24, 12, 27, 3], [12, 8, 3, 36, 27]));
+console.log(getCommonElements([10, 20, 30, 40], [4, 30, 17, 10, 40]));
+console.log(getCommonElements([1, 2, 3], [10, 20, 30]));
