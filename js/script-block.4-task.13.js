5fae89f39a070e58148e53284fe6c1a362c27c55
Vadym Kalinchenkov <midavkk@gmail.com>
2022-11-10 19:07:08 +0200

    Update script-block.4-task.13.js

--- js/script-block.4-task.13.js
+++ js/script-block.4-task.13.js
@@ -1,2 +1,53 @@
 // <<<------------- EXAMPLE ------------->>>
-// <<<------------- TASK ------------->>>
+
+// Функция с побочными эффектами
+// const dirtyMultiply = (array, value) => {
+//   //   console.log(array);
+//   for (let i = 0; i < array.length; i += 1) {
+//     array[i] = array[i] * value;
+//   }
+// };
+
+// const numbers = [1, 2, 3, 4, 5];
+// dirtyMultiply(numbers, 2);
+// // Произошла мутация исходных данных - массива numbers
+// console.log(numbers); // [2, 4, 6, 8, 10]
+
+// Чистая функция (pure function)
+// const pureMultiply = (array, value) => {
+//   const newArray = [];
+
+//   array.forEach((element) => {
+//     newArray.push(element * value);
+//   });
+
+//   return newArray;
+// };
+
+// const numbers = [1, 2, 3, 4, 5];
+// const doubledNumbers = pureMultiply(numbers, 2);
+
+// // Не произошло мутации исходных данных
+// console.log(numbers); // [1, 2, 3, 4, 5]
+// // Функция вернула новый массив с изменёнными данными
+// console.log(doubledNumbers); // [2, 4, 6, 8, 10]
+
+// <<<------------- TASK - ЧИСТЫЕ ФУНКЦИИ ------------->>>
+
+const changeEven = (numbers, value) => {
+  // Change code below this line
+  const newArray = [];
+  //   for (let i = 0; i < numbers.length; i += 1) {
+  //     if (numbers[i] % 2 === 0) {
+  //       numbers[i] = numbers[i] + value;
+  //     }
+  //     }
+  numbers.forEach((element) => {
+    if (element % 2 === 0) {
+      newArray.push(element + value);
+    }
+  });
+  return newArray;
+  // Change code above this line
+};
+console.log(changeEven([1, 2, 3, 4, 5], 10));
