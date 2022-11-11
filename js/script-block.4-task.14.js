fd3ba5b2a28928fb60156be1dbe7229c650a9de2
Vadym Kalinchenkov <midavkk@gmail.com>
2022-11-11 12:15:25 +0200

    Update script-block.4-task.14.js

--- js/script-block.4-task.14.js
+++ js/script-block.4-task.14.js
@@ -1,2 +1,18 @@
 // <<<------------- EXAMPLE ------------->>>
-// <<<------------- TASK ------------->>>
+// const planets = ["Земля", "Марс", "Венера", "Юпитер"];
+
+// const planetsInUpperCase = planets.map((planet) => planet.toUpperCase());
+// console.log(planetsInUpperCase); // ['ЗЕМЛЯ', 'МАРС', 'ВЕНЕРА', 'ЮПИТЕР']
+
+// const planetsInLowerCase = planets.map((planet) => planet.toLowerCase());
+// console.log(planetsInLowerCase); // ['земля', 'марс', 'венера', 'юпитер']
+
+// // Оригинальный массив не изменился
+// console.log(planets); // ['Земля', 'Марс', 'Венера', 'Юпитер']
+
+// <<<------------- TASK - Метод map() ------------->>>
+const planets = ["Earth", "Mars", "Venus", "Jupiter"];
+// Change code below this line
+const planetsLengths = planets.map((planet) => planet.length);
+console.log(planetsLengths);
+console.log(planets);
