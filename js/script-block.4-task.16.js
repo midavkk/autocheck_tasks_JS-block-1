fd3e220b2cda418ed925c44c0cc80485549e1ff2
Vadym Kalinchenkov <midavkk@gmail.com>
2022-11-11 12:15:35 +0200

    Update script-block.4-task.16.js

--- js/script-block.4-task.16.js
+++ js/script-block.4-task.16.js
@@ -1,2 +1,37 @@
 // <<<------------- EXAMPLE ------------->>>
-// <<<------------- TASK ------------->>>
+// const students = [
+//   { name: "Манго", courses: ["математика", "физика"] },
+//   { name: "Поли", courses: ["информатика", "математика"] },
+//   { name: "Киви", courses: ["физика", "биология"] },
+// ];
+
+// const courses = students.map((student) => student.courses);
+// console.log(courses);
+// // [['математика', 'физика'], ['информатика', 'математика'], ['физика', 'биология']]
+
+// const flatCourses = students.flatMap((student) => student.courses);
+// console.log(flatCourses);
+// // ['математика', 'физика', 'информатика', 'математика', 'физика', 'биология'];
+
+// <<<------------- TASK - Метод flatMap() ------------->>>
+const books = [
+  {
+    title: "The Last Kingdom",
+    author: "Bernard Cornwell",
+    genres: ["adventure", "history"],
+  },
+  {
+    title: "Beside Still Waters",
+    author: "Robert Sheckley",
+    genres: ["fiction"],
+  },
+  {
+    title: "Redder Than Blood",
+    author: "Tanith Lee",
+    genres: ["horror", "mysticism"],
+  },
+];
+// Change code below this line
+
+const genres = books.flatMap((book) => book.genres);
+console.log(genres);
