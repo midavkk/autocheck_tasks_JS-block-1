854a8c812dace09457eb3b76d9ef9ab92911df66
Vadym Kalinchenkov <midavkk@gmail.com>
2022-11-11 12:15:30 +0200

    Update script-block.4-task.15.js

--- js/script-block.4-task.15.js
+++ js/script-block.4-task.15.js
@@ -1,2 +1,36 @@
 // <<<------------- EXAMPLE ------------->>>
-// <<<------------- TASK ------------->>>
+// const students = [
+//   { name: "Манго", score: 83 },
+//   { name: "Поли", score: 59 },
+//   { name: "Аякс", score: 37 },
+//   { name: "Киви", score: 94 },
+//   { name: "Хьюстон", score: 64 },
+// ];
+
+// const names = students.map((student) => student.name);
+// console.log(names); // ['Манго', 'Поли', 'Аякс', 'Киви', 'Хьюстон']
+
+// <<<------------- TASK - Массив объектов Метод map() ------------->>>
+const books = [
+  {
+    title: "The Last Kingdom",
+    author: "Bernard Cornwell",
+    rating: 8.38,
+  },
+  {
+    title: "Beside Still Waters",
+    author: "Robert Sheckley",
+    rating: 8.51,
+  },
+  {
+    title: "The Dream of a Ridiculous Man",
+    author: "Fyodor Dostoevsky",
+    rating: 7.75,
+  },
+  { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+  { title: "Enemy of God", author: "Bernard Cornwell", rating: 8.67 },
+];
+// Change code below this line
+
+const titles = books.map((book) => book.title);
+console.log(titles);
