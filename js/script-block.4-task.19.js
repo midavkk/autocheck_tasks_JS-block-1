b78597a547c3c4bc4aaeb57d1fa77924546974aa
Vadym Kalinchenkov <midavkk@gmail.com>
2022-11-16 18:39:35 +0200

    Update script-block.4-task.19.js

--- js/script-block.4-task.19.js
+++ js/script-block.4-task.19.js
@@ -1,2 +1,21 @@
 // <<<------------- EXAMPLE ------------->>>
+// const values = [51, -3, 27, 21, -68, 42, -37];
+
+// const positiveValues = values.filter((value) => value >= 0);
+// console.log(positiveValues); // [51, 27, 21, 42]
+
+// const negativeValues = values.filter((value) => value < 0);
+// console.log(negativeValues); // [-3, -68, -37]
+
+// const bigValues = values.filter((value) => value > 1000);
+// console.log(bigValues); // []
+
+// // Оригинальный массив не изменился
+// console.log(values); // [51, -3, 27, 21, -68, 42, -37]
+
 // <<<------------- TASK ------------->>>
+const numbers = [17, 24, 82, 61, 36, 18, 47, 52, 73];
+// Change code below this line
+
+const evenNumbers = numbers;
+const oddNumbers = numbers;
