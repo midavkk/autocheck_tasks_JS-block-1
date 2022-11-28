755b9e9e77ad4241f4ead1442992a2034b57f6b1
Vadym Kalinchenkov <midavkk@gmail.com>
2022-11-29 01:18:11 +0200

    Update script-block.4-task.21.js

--- js/script-block.4-task.21.js
+++ js/script-block.4-task.21.js
@@ -1,2 +1,24 @@
 // <<<------------- EXAMPLE ------------->>>
+const LOW_SCORE = 50;
+const HIGH_SCORE = 80;
+const students = [
+  { name: "Mango", score: 83 },
+  { name: "Poly", score: 59 },
+  { name: "Ajax", score: 37 },
+  { name: "Kiwi", score: 94 },
+  { name: "Houston", score: 64 },
+];
+
+const best = students.filter((student) => student.score >= HIGH_SCORE);
+console.log(best); // Массив объектов с именами Mango и Kiwi
+
+const worst = students.filter((student) => student.score < LOW_SCORE);
+console.log(worst); // Массив с одним объектом Ajax
+
+// В коллбек-функции удобно деструктуризировать свойства объекта
+const average = students.filter(
+  ({ score }) => score >= LOW_SCORE && score < HIGH_SCORE
+);
+console.log(average); // Массив объектов с именами Poly и Houston
+
 // <<<------------- TASK ------------->>>
